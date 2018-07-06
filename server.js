5fc4a2967147c3648664ecfb790490c1ce295b63
Andy <anlaics2@gmail.com>
2018-07-06 09:42:20 -0700

    initialize user route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+
 const app = express();
 
 // DB config
@@ -13,6 +15,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+// User Routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
