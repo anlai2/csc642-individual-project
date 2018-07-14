b14ee3e788a3b40bdae9c68694829cc8a474a6de
Andy <anlaics2@gmail.com>
2018-07-14 12:00:01 -0700

    redux and routing initalized

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Register from './components/Register';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Route exact path="/" component={Register} />
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/components/Register.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import TextFieldGroup from './common/TextFieldGroup';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      zipcode: '',
+      education: '',
+      income: '',
+      phone: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            CSC 642 Summer 2018 Individual Assignment Andy Lai
+          </h1>
+        </header>
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h3 className="display-4 text-center mb-4 mt-4">Register</h3>
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <TextFieldGroup
+                      placeholder="First Name"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.onChange}
+                      error={errors.firstName}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Email Address"
+                      name="email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                      name="password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Confirm Password"
+                      name="password2"
+                    />
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
