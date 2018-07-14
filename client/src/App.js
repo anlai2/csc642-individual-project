7f36da3d05c3ecc03f0da3e83ae9b20f72ed7423
Andy <anlaics2@gmail.com>
2018-07-14 13:02:12 -0700

    add footer and recaptcha

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Route exact path="/" component={Register} />
-        </Router>
+        <div className="App">
+          <Router>
+            <Route exact path="/" component={Register} />
+          </Router>
+        </div>
       </Provider>
     );
   }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import TextFieldGroup from './common/TextFieldGroup';
+import Footer from './layout/Footer';
 
 class Register extends Component {
   constructor() {
@@ -15,6 +17,7 @@ class Register extends Component {
       email: '',
       password: '',
       password2: '',
+      recaptcha: false,
       errors: {}
     };
   }
@@ -28,20 +31,21 @@ class Register extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">
-            CSC 642 Summer 2018 Individual Assignment Andy Lai
-          </h1>
+          <h1>CSC 642 Summer 2018 Individual Assignment Andy Lai</h1>
         </header>
         <div className="register">
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
-                <h3 className="display-4 text-center mb-4 mt-4">Register</h3>
+                <h3 className="display-4 text-center mb-4 mt-4">
+                  Registration Form
+                </h3>
+                <p className="d-block pb-3">* = required fields</p>
                 <form onSubmit={this.onSubmit}>
                   <div className="row">
                     <div className="col">
                       <TextFieldGroup
-                        placeholder="First Name"
+                        placeholder="* First Name"
                         name="firstName"
                         value={this.state.firstName}
                         onChange={this.onChange}
@@ -50,7 +54,7 @@ class Register extends Component {
                     </div>
                     <div className="col">
                       <TextFieldGroup
-                        placeholder="Last Name"
+                        placeholder="* Last Name"
                         name="lastName"
                         value={this.state.lastName}
                         onChange={this.onChange}
@@ -58,39 +62,99 @@ class Register extends Component {
                       />
                     </div>
                   </div>
+                  <hr />
                   <div className="form-group">
-                    <input
-                      type="email"
-                      className="form-control form-control-lg"
-                      placeholder="Email Address"
-                      name="email"
+                    <TextFieldGroup
+                      placeholder="* Address"
+                      name="address"
+                      value={this.state.address}
+                      onChange={this.onChange}
+                      error={errors.address}
                     />
                   </div>
                   <div className="form-group">
-                    <input
-                      type="password"
-                      className="form-control form-control-lg"
-                      placeholder="Password"
+                    <TextFieldGroup
+                      placeholder="* Zipcode"
+                      name="zipcode"
+                      value={this.state.zipcode}
+                      onChange={this.onChange}
+                      error={errors.zipcode}
+                    />
+                  </div>
+                  <hr />
+                  <div className="form-group">
+                    <TextFieldGroup
+                      placeholder="Education"
+                      name="education"
+                      value={this.state.education}
+                      onChange={this.onChange}
+                      error={errors.education}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <TextFieldGroup
+                      placeholder="Income"
+                      name="income"
+                      value={this.state.income}
+                      onChange={this.onChange}
+                      error={errors.income}
+                    />
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col">
+                      <TextFieldGroup
+                        placeholder="* Phone Number"
+                        name="phone"
+                        value={this.state.phone}
+                        onChange={this.onChange}
+                        error={errors.phone}
+                      />
+                    </div>
+                    <div className="col">
+                      <TextFieldGroup
+                        placeholder="Email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        error={errors.email}
+                      />
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="form-group">
+                    <TextFieldGroup
+                      placeholder="* Password"
                       name="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                      error={errors.password}
                     />
                   </div>
                   <div className="form-group">
-                    <input
-                      type="password"
-                      className="form-control form-control-lg"
-                      placeholder="Confirm Password"
+                    <TextFieldGroup
+                      placeholder="* Confirm Password"
                       name="password2"
+                      value={this.state.password2}
+                      onChange={this.onChange}
+                      error={errors.password2}
                     />
                   </div>
+                  <ReCAPTCHA
+                    ref="recaptcha"
+                    sitekey="6LdA7GMUAAAAANYVPP-K1G8PQu-yjMtYp5yE3MGG"
+                    onChange={() => this.setState({ recaptcha: true })}
+                  />
                   <input
                     type="submit"
-                    className="btn btn-info btn-block mt-4"
+                    className="btn btn-primary btn-block mt-4"
                   />
                 </form>
               </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
