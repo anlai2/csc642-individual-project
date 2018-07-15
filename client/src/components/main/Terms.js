00c364bf0d6901c98fd048ce941ea2ae6bac7bf4
Andy <anlaics2@gmail.com>
2018-07-15 14:56:12 -0700

    style terms header

--- client/src/components/main/Terms.js
+++ client/src/components/main/Terms.js
@@ -8,7 +8,9 @@ class Terms extends Component {
           <h2>CSC 642 Summer 2018 Individual Assignment Andy Lai</h2>
         </header>
         <div className="col-md-12">
-          <h3>Terms and Conditions</h3>
+          <h3 className="display-4 text-center mb-4 mt-4">
+            Terms and Conditions
+          </h3>
           <h5>We will never share your information with anyone.</h5>
         </div>
         <Footer />
